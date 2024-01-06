58474b5640f2ffbc537b06f93b6b1c8989f526d7
yahyakamran <yahyakamran099@gmail.com>
2024-01-06 05:06:39 +0500

    Implemented createElement

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
-const element = {
+/*
+  //React With Plain Js
+  const element = {
     type : "h1",
     props: {
         title    : "Foo",
@@ -6,9 +8,50 @@ const element = {
     },
 };
 const node = document.createElement(element.type);
-node["title"] = element.props.title;
+node["title"] = element.prop.title;
 const text = document.createTextNode(element.props.children);
 node.appendChild(text)
 const root = document.getElementById("root");
-root.appendChild(node)
+root.appendChild(node)*/
+
+
+import {createRoot} from "react-dom/client";
+
+function createTextElement(text){
+    return{
+        type  : "TEXT_ELEMENT",
+        props : {
+            nodeValue :text,
+            children : [],
+        }
+    }
+}
+
+function createElement( type , props , ...children ){
+    return{
+        type,
+        props : {
+            ...props,
+                children : children.map(child =>
+                typeof child == "object"
+                ? child
+                : createTextElement(child)
+            ),
+        },
+    }
+}
+
+const Reacto = {
+    createElement,
+}
+/** @jsx Reacto.createElement */
+const element = Reacto.createElement(
+    <div id="bar" >
+        <p>foo</p>
+        <a/>
+    </div>
+);
+const root = createRoot(document.getElementById("root"));
+root.render(element)
+
 
